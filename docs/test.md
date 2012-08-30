6ad4922b0945b0161591e7beb425d48ee0b12b47
Oskar Schöldström <public@oxy.fi>
2012-08-30 14:35:14 +0300

    Make tests work in cli and browser

---
+++ docs/test.md
@@ -0,0 +1,18 @@
+# TOC
+   - [test suite](#test-suite)
+<a name="" />
+ 
+<a name="test-suite" />
+# test suite
+should expose a function.
+
+```js
+expect(add).to.be.a('function');
+```
+
+should do math.
+
+```js
+expect(add(1, 3)).to.equal(4);
+```
+
--- test/example.js
+++ test/example.js
@@ -1,9 +1,15 @@
-var assert = require('assert');
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
+if (typeof expect === 'undefined') {
+  var expect = require('expect.js');
+}
+
+function add (a, b) { return a + b; }
+
+describe('test suite', function () {
+  it('should expose a function', function () {
+    expect(add).to.be.a('function');
+  });
+
+  it('should do math', function () {
+    expect(add(1, 3)).to.equal(4);
   });
 });
