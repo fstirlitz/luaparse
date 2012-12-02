0b20c48a59a3bbbe9cbce0ed34c7b05e914f197f
Oskar Schöldström <public@oxy.fi>
2012-12-02 18:45:13 +0200

    Cleanup dead code

--- test/spec/misc.js
+++ test/spec/misc.js
@@ -1,12 +1,22 @@
 describe('API', function () {
-  it('should have a parse function', function () {
+  it('should have parse functions', function () {
     expect(parser).to.be.a('object');
     expect(parser.parse).to.be.a('function');
+    expect(parser.write).to.be.a('function');
+    expect(parser.end).to.be.a('function');
   });
-  it('should not crash on only whitespace body', function() {
-    expectObj(parser.parse(' '), {
+  it('should produce empty tree on empty input', function() {
+    expectObj(parser.parse(''), {
         type: 'Chunk'
       , body: []
+      , comments: []
+    });
+  });
+  it('should support waiting on input', function () {
+    var parse = parser.parse({ wait: true });
+    expectObj(parse.end('break'), {
+        type: 'Chunk'
+      , body: [{ type: 'BreakStatement' }]
       , comments: []
     });
   });
