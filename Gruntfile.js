f92469767df18ed387d9dc3f47b6c0708c78b864
Oskar Schöldström <public@oxy.fi>
2013-03-29 15:38:29 +0200

    Use Grunt 0.4

---
+++ Gruntfile.js
@@ -0,0 +1,93 @@
+/*globals module:true require:true */
+module.exports = function (grunt) {
+  'use strict';
+
+  // With this we can still override options.
+  function readOptionalJSON(filepath) {
+    var data = {};
+    try {
+      data = grunt.file.readJSON(filepath);
+      grunt.verbose.write('Reading ' + filepath + '...').ok();
+    } catch(e) {}
+    return data;
+  }
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    concat: {
+      options: {
+        stripBanners: true,
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+          '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+          ' * <%= pkg.homepage %>\n' +
+          ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+          ' Licensed MIT\n' +
+          ' */\n'
+      },
+      dist: {
+        src: ['lib/luaparse.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+
+    uglify: {
+      options: { report: 'gzip' },
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js' : ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+
+    jshint: {
+      options: { jshintrc: '.jshintrc' },
+      browser: {
+        options: { browser: true, globals: { define: true } },
+        src: [
+          'lib/*.js',
+          'examples/**/*.js',
+          'benchmarks/*.js'
+        ]
+      },
+      node: {
+        options: { node: true },
+        src: [
+          'package.json',
+          'Gruntfile.js',
+          'scripts/make-test',
+          'scripts/pre-commit',
+          'bin/*'
+        ]
+      },
+      test: {
+        options: { jshintrc: 'test/spec/.jshintrc' },
+        src: ['test/spec/**/*.js']
+      }
+    },
+
+    inlinelint: {
+      options: readOptionalJSON('.jshintrc'),
+      html: {
+        options: {
+          browser: true,
+          laxcomma: true,
+          globals: { YUI: true, prettyPrint: true, jQuery: true, luaparse:true }
+        },
+        src: ['examples/**/*.html', 'docs/**/*.html']
+      }
+    },
+
+    bashlint: {
+      src: ['**/*.sh']
+    }
+  });
+
+  Object.keys(require('./package.json').devDependencies).forEach(function(dep) {
+    if (dep.substring(0,6) === 'grunt-' && dep !== 'grunt-cli') grunt.loadNpmTasks(dep);
+  });
+
+  grunt.registerTask('lint', ['jshint', 'inlinelint', 'bashlint']);
+  grunt.registerTask('build', ['concat', 'uglify']);
+  grunt.registerTask('default', ['concat', 'uglify', 'lint']);
+};
