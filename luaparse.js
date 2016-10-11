ab478b94caa3bc935015f6df96837dd43bc0205e
Marius Petcu <dapetcu21@gmail.com>
2016-10-11 12:20:48 +0300

    Add onLocalDeclaration callback

--- luaparse.js
+++ luaparse.js
@@ -74,6 +74,9 @@
     , onCreateScope: null
     // A callback which will be invoked when the current scope is destroyed.
     , onDestroyScope: null
+    // A callback which will be invoked when a local variable is declared in the current scope.
+    // The variable's name will be passed as the only parameter
+    , onLocalDeclaration: null
     // The version of Lua targeted by the parser (string; allowed values are '5.1', '5.2', '5.3'.)
     , luaVersion: '5.1'
   };
@@ -1262,6 +1265,7 @@
 
   // Add identifier name to the current scope if it doesnt already exist.
   function scopeIdentifierName(name) {
+    if (options.onLocalDeclaration) options.onLocalDeclaration(name);
     if (-1 !== indexOf(scopes[scopeDepth], name)) return;
     scopes[scopeDepth].push(name);
   }
