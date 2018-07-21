4a2d0cbb2bf2c06932748c8d97795a883138849d
Travis <public@oxy.fi>
2018-07-21 08:15:05 +0000

    build documentation

--- test/spec/statements.js
+++ test/spec/statements.js
@@ -109,6 +109,126 @@
       "result": "[1:0] unexpected symbol ';' near '<eof>'"
     },
     {
+      "source": "goto \"foo\"",
+      "result": {
+        "type": "Chunk",
+        "body": [
+          {
+            "type": "CallStatement",
+            "expression": {
+              "type": "StringCallExpression",
+              "base": {
+                "type": "Identifier",
+                "name": "goto",
+                "loc": {
+                  "start": {
+                    "line": 1,
+                    "column": 0
+                  },
+                  "end": {
+                    "line": 1,
+                    "column": 4
+                  }
+                },
+                "range": [
+                  0,
+                  4
+                ],
+                "isLocal": false
+              },
+              "argument": {
+                "type": "StringLiteral",
+                "value": "foo",
+                "raw": "\"foo\"",
+                "loc": {
+                  "start": {
+                    "line": 1,
+                    "column": 5
+                  },
+                  "end": {
+                    "line": 1,
+                    "column": 10
+                  }
+                },
+                "range": [
+                  5,
+                  10
+                ]
+              },
+              "loc": {
+                "start": {
+                  "line": 1,
+                  "column": 0
+                },
+                "end": {
+                  "line": 1,
+                  "column": 10
+                }
+              },
+              "range": [
+                0,
+                10
+              ]
+            },
+            "loc": {
+              "start": {
+                "line": 1,
+                "column": 0
+              },
+              "end": {
+                "line": 1,
+                "column": 10
+              }
+            },
+            "range": [
+              0,
+              10
+            ]
+          }
+        ],
+        "loc": {
+          "start": {
+            "line": 1,
+            "column": 0
+          },
+          "end": {
+            "line": 1,
+            "column": 10
+          }
+        },
+        "range": [
+          0,
+          10
+        ],
+        "comments": [],
+        "globals": [
+          {
+            "type": "Identifier",
+            "name": "goto",
+            "loc": {
+              "start": {
+                "line": 1,
+                "column": 0
+              },
+              "end": {
+                "line": 1,
+                "column": 4
+              }
+            },
+            "range": [
+              0,
+              4
+            ],
+            "isLocal": false
+          }
+        ]
+      }
+    },
+    {
+      "source": "goto foo",
+      "result": "[1:0] unexpected identifier 'goto' near '<eof>'"
+    },
+    {
       "source": "return;",
       "result": {
         "type": "Chunk",
@@ -277,8 +397,8 @@
       }
     },
     {
-      "source": "goto",
-      "result": "[1:4] <name> expected near '<eof>'",
+      "source": "goto \"foo\"",
+      "result": "[1:5] <name> expected near 'foo'",
       "options": {
         "comments": true,
         "locations": true,
@@ -351,6 +471,273 @@
         "ranges": true,
         "scope": true,
         "luaVersion": "5.2"
+      }
+    },
+    {
+      "source": "::foo::",
+      "result": {
+        "type": "Chunk",
+        "body": [
+          {
+            "type": "LabelStatement",
+            "label": {
+              "type": "Identifier",
+              "name": "foo",
+              "loc": {
+                "start": {
+                  "line": 1,
+                  "column": 2
+                },
+                "end": {
+                  "line": 1,
+                  "column": 5
+                }
+              },
+              "range": [
+                2,
+                5
+              ],
+              "isLocal": true
+            },
+            "loc": {
+              "start": {
+                "line": 1,
+                "column": 0
+              },
+              "end": {
+                "line": 1,
+                "column": 7
+              }
+            },
+            "range": [
+              0,
+              7
+            ]
+          }
+        ],
+        "loc": {
+          "start": {
+            "line": 1,
+            "column": 0
+          },
+          "end": {
+            "line": 1,
+            "column": 7
+          }
+        },
+        "range": [
+          0,
+          7
+        ],
+        "comments": [],
+        "globals": []
+      },
+      "options": {
+        "comments": true,
+        "locations": true,
+        "ranges": true,
+        "scope": true,
+        "luaVersion": "LuaJIT"
+      }
+    },
+    {
+      "source": "goto \"foo\"",
+      "result": {
+        "type": "Chunk",
+        "body": [
+          {
+            "type": "CallStatement",
+            "expression": {
+              "type": "StringCallExpression",
+              "base": {
+                "type": "Identifier",
+                "name": "goto",
+                "loc": {
+                  "start": {
+                    "line": 1,
+                    "column": 0
+                  },
+                  "end": {
+                    "line": 1,
+                    "column": 4
+                  }
+                },
+                "range": [
+                  0,
+                  4
+                ],
+                "isLocal": false
+              },
+              "argument": {
+                "type": "StringLiteral",
+                "value": "foo",
+                "raw": "\"foo\"",
+                "loc": {
+                  "start": {
+                    "line": 1,
+                    "column": 5
+                  },
+                  "end": {
+                    "line": 1,
+                    "column": 10
+                  }
+                },
+                "range": [
+                  5,
+                  10
+                ]
+              },
+              "loc": {
+                "start": {
+                  "line": 1,
+                  "column": 0
+                },
+                "end": {
+                  "line": 1,
+                  "column": 10
+                }
+              },
+              "range": [
+                0,
+                10
+              ]
+            },
+            "loc": {
+              "start": {
+                "line": 1,
+                "column": 0
+              },
+              "end": {
+                "line": 1,
+                "column": 10
+              }
+            },
+            "range": [
+              0,
+              10
+            ]
+          }
+        ],
+        "loc": {
+          "start": {
+            "line": 1,
+            "column": 0
+          },
+          "end": {
+            "line": 1,
+            "column": 10
+          }
+        },
+        "range": [
+          0,
+          10
+        ],
+        "comments": [],
+        "globals": [
+          {
+            "type": "Identifier",
+            "name": "goto",
+            "loc": {
+              "start": {
+                "line": 1,
+                "column": 0
+              },
+              "end": {
+                "line": 1,
+                "column": 4
+              }
+            },
+            "range": [
+              0,
+              4
+            ],
+            "isLocal": false
+          }
+        ]
+      },
+      "options": {
+        "comments": true,
+        "locations": true,
+        "ranges": true,
+        "scope": true,
+        "luaVersion": "LuaJIT"
+      }
+    },
+    {
+      "source": "goto foo",
+      "result": {
+        "type": "Chunk",
+        "body": [
+          {
+            "type": "GotoStatement",
+            "label": {
+              "type": "Identifier",
+              "name": "foo",
+              "loc": {
+                "start": {
+                  "line": 1,
+                  "column": 5
+                },
+                "end": {
+                  "line": 1,
+                  "column": 8
+                }
+              },
+              "range": [
+                5,
+                8
+              ]
+            },
+            "loc": {
+              "start": {
+                "line": 1,
+                "column": 0
+              },
+              "end": {
+                "line": 1,
+                "column": 8
+              }
+            },
+            "range": [
+              0,
+              8
+            ]
+          }
+        ],
+        "loc": {
+          "start": {
+            "line": 1,
+            "column": 0
+          },
+          "end": {
+            "line": 1,
+            "column": 8
+          }
+        },
+        "range": [
+          0,
+          8
+        ],
+        "comments": [],
+        "globals": []
+      },
+      "options": {
+        "comments": true,
+        "locations": true,
+        "ranges": true,
+        "scope": true,
+        "luaVersion": "LuaJIT"
+      }
+    },
+    {
+      "source": "goto goto",
+      "result": "[1:0] unexpected identifier 'goto' near '<eof>'",
+      "options": {
+        "comments": true,
+        "locations": true,
+        "ranges": true,
+        "scope": true,
+        "luaVersion": "LuaJIT"
       }
     }
   ];
